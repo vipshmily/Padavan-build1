e19705c454bc653903b2a9d4835b45909d1b30d2
vipshmily <85991289+vipshmily@users.noreply.github.com>
2021-11-18 21:37:30 +0800

    Update shadowsocks.sh

--- public/shadowsocks.sh
+++ public/shadowsocks.sh
@@ -15,12 +15,15 @@ CONFIG_UDP_FILE=/tmp/${NAME}_u.json
 CONFIG_SOCK5_FILE=/tmp/${NAME}_s.json
 CONFIG_KUMASOCKS_FILE=/tmp/kumasocks.toml
 v2_json_file="/tmp/v2-redir.json"
+#xray_json_file="/tmp/xr-redir.json"
 trojan_json_file="/tmp/tj-redir.json"
 v2_bin="/usr/bin/v2ray"
+xray_bin="/usr/bin/xray"
 tj_bin="/usr/bin/trojan"
 server_count=0
 redir_tcp=0
 v2ray_enable=0
+#xray_enable=0
 redir_udp=0
 tunnel_enable=0
 local_enable=0
@@ -72,17 +75,17 @@ local type=$stype
 		tj_bin="/usr/bin/trojan"
 		if [ ! -f "$tj_bin" ]; then
 		if [ ! -f "/tmp/trojan" ]; then
-			curl -L -k -s -o /tmp/trojan --connect-timeout 10 --retry 3 https://cdn.jsdelivr.net/gh/eprea/cdn/trojan
+			curl -L -k -s -o /tmp/trojan --connect-timeout 10 --retry 3 https://cdn.jsdelivr.net/gh/vipshmily/OutSide/trojan
 			if [ ! -f "/tmp/trojan" ]; then
-				logger -t "SS" "trojan二进制文件下载失败，可能是地址失效或者网络异常！准备切换备用下载地址！"
+				logger -t "SS" "trojan二进制文件下载失败，可能是地址失效或者网络异常！自动切换到备用下载！"
 				#curl -L -k -s -o /tmp/trojan --connect-timeout 10 --retry 3 https://bin.wololo.vercel.app/trojan
-				curl -L -k -s -o /tmp/trojan --connect-timeout 10 --retry 3 https://ghproxy.com/https://github.com/eprea/cdn/blob/master/trojan
+				curl -L -k -s -o /tmp/trojan --connect-timeout 10 --retry 3 https://ghproxy.com/https://github.com/vipshmily/OutSide/blob/main/trojan
 				if [ ! -f "/tmp/trojan" ]; then
-					logger -t "SS" "trojan二进制文件备用地址下载失败！请自查网络！"
+					logger -t "SS" "trojan二进制文件备用下载失败！请自查网络！"
 					nvram set ss_enable=0
 					ssp_close
 				else
-					logger -t "SS" "trojan二进制文件备用地址下载成功"
+					logger -t "SS" "trojan二进制文件备用下载成功"
 					chmod -R 777 /tmp/trojan
 					tj_bin="/tmp/trojan"
 				fi
@@ -107,16 +110,16 @@ local type=$stype
 		v2_bin="/usr/bin/v2ray"
 		if [ ! -f "$v2_bin" ]; then
 		if [ ! -f "/tmp/v2ray" ]; then
-			curl -L -k -s -o /tmp/v2ray --connect-timeout 10 --retry 3 https://cdn.jsdelivr.net/gh/eprea/cdn/xray
+			curl -L -k -s -o /tmp/v2ray --connect-timeout 10 --retry 3 https://cdn.jsdelivr.net/gh/vipshmily/OutSide/xray
 			if [ ! -f "/tmp/v2ray" ]; then
-				logger -t "SS" "v2ray二进制文件下载失败，可能是地址失效或者网络异常！准备切换备用下载地址！"
-				curl -L -k -s -o /tmp/v2ray --connect-timeout 10 --retry 3 https://ghproxy.com/https://github.com/eprea/cdn/blob/master/xray
+				logger -t "SS" "v2ray二进制文件下载失败，可能是地址失效或者网络异常！自动切换到备用下载！"
+				curl -L -k -s -o /tmp/v2ray --connect-timeout 10 --retry 3 https://ghproxy.com/https://github.com/vipshmily/OutSide/blob/main/xray
 				if [ ! -f "/tmp/v2ray" ]; then
-					logger -t "SS" "v2ray二进制文件备用地址下载失败！请自查网络！"
+					logger -t "SS" "v2ray二进制文件备用下载失败！请自查网络！"
 					nvram set ss_enable=0
 					ssp_close
 				else
-					logger -t "SS" "v2ray二进制文件备用地址下载成功"
+					logger -t "SS" "v2ray二进制文件备用下载成功"
 					chmod -R 777 /tmp/v2ray
 					v2_bin="/tmp/v2ray"
 				fi
@@ -142,16 +145,16 @@ local type=$stype
 		v2_bin="/usr/bin/v2ray"
 		if [ ! -f "$v2_bin" ]; then
 		if [ ! -f "/tmp/v2ray" ]; then
-			curl -L -k -s -o /tmp/v2ray --connect-timeout 10 --retry 3 https://cdn.jsdelivr.net/gh/eprea/cdn/xray
+			curl -L -k -s -o /tmp/v2ray --connect-timeout 10 --retry 3 https://cdn.jsdelivr.net/gh/vipshmily/OutSide/xray
 			if [ ! -f "/tmp/v2ray" ]; then
-				logger -t "SS" "v2ray二进制文件下载失败，可能是地址失效或者网络异常！准备切换备用下载地址！"
-				curl -L -k -s -o /tmp/v2ray --connect-timeout 10 --retry 3 https://ghproxy.com/https://github.com/eprea/cdn/blob/master/xray
+				logger -t "SS" "v2ray二进制文件下载失败，可能是地址失效或者网络异常！自动切换到备用下载！"
+				curl -L -k -s -o /tmp/v2ray --connect-timeout 10 --retry 3 https://ghproxy.com/https://github.com/vipshmily/OutSide/blob/main/xray
 				if [ ! -f "/tmp/v2ray" ]; then
-					logger -t "SS" "v2ray二进制文件备用地址下载失败！请自查网络！"
+					logger -t "SS" "v2ray二进制文件备用下载失败！请自查网络！"
 					nvram set ss_enable=0
 					ssp_close
 				else
-					logger -t "SS" "v2ray二进制文件备用地址下载成功"
+					logger -t "SS" "v2ray二进制文件备用下载成功"
 					chmod -R 777 /tmp/v2ray
 					v2_bin="/tmp/v2ray"
 				fi
@@ -370,7 +373,7 @@ case "$run_mode" in
 		if [ $(nvram get ss_chdns) = 1 ]; then
 			chinadnsng_enable_flag=1
 			logger -t "SS" "下载cdn域名文件..."
-			wget --no-check-certificate --timeout=8 -qO - https://gitee.com/bkye/rules/raw/master/cdn.txt > /tmp/cdn.txt
+			wget --no-check-certificate --timeout=8 -qO - https://raw.githubusercontent.com/hq450/fancyss/master/rules/cdn.txt > /tmp/cdn.txt
 			if [ ! -f "/tmp/cdn.txt" ]; then
 				logger -t "SS" "cdn域名文件下载失败，可能是地址失效或者网络异常！可能会影响部分国内域名解析了国外的IP！"
 			else
